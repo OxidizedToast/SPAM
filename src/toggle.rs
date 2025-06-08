f3ef3db3f6e16b2ad8d29d1c7231efb13606cdbe
OxidizedToast <faraday2122@protonmail.com>
2025-06-07 21:24:54 -0500

    Minor commit to toggle.rs and closed #7

--- src/toggle.rs
+++ src/toggle.rs
@@ -16,14 +16,14 @@ pub fn toggle(){
     let daemon_actived = config_file::check_file_contents();
     // Confirms the directory exists
     fs::create_dir_all(&config_dir_path).unwrap();
+
+    // toggles the config files boolean value
     if daemon_actived == true{
-        // make file read to false
         println!("Toggling S.P.A.M Off");
 
         let mut file = fs::File::create(&config_file_path).unwrap();
         file.write_all(b"toggled = false").unwrap();
     } else if daemon_actived == false {
-       // make file read to true 
         println!("Toggling S.P.A.M On");
 
         let mut file = fs::File::create(&config_file_path).unwrap();
